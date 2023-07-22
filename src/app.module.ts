dbae44823800b1be452e7e13d4f233f3deec6d79
riccardotornesello <riccardo.tornesello@gmail.com>
2023-07-22 15:38:41 +0400

    remove mongo module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,29 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 // Common modules
 import { ConfigModule } from './common/config/config.module';
-import { MongoModule } from './common/mongo/mongo.module';
 import { CacheModule } from './common/cache/cache.module';
 import { AuthModule } from './common/auth/auth.module';
 import { UsersModule } from './common/users/users.module';
 
 @Module({
-  imports: [ConfigModule, MongoModule, CacheModule, AuthModule, UsersModule],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('mongo.url'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule,
+    CacheModule,
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
